21ea5f939b265c6cb0b4998ead10c8c00d785d88
Augusto Inacio <153130112+Od0g@users.noreply.github.com>
2024-02-01 19:32:33 -0300

    Add files via upload
    Feitos no 31/01/2024

---
+++ ola_mundo.py
@@ -0,0 +1,16 @@
+#print("Olá Mundo !")
+print("Informe seu nome: ")
+nome= input()
+print("Informe seu idade: ")
+idade= int(input())
+
+print("seu nome é: ", nome)
+print("sua idade é: ", idade)
+print("tipo da variavel nome: ", type(nome))
+print("tipo da variavel idade: ", type(idade))
+
+'''
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+print("Olá, " + nome + " a sua idade é de: " + idade + " anos.")
+'''
